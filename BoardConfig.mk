d12a4b00a7362b11d43d46333b2d355e8b917f49
Alexander Koskovich <akoskovich@pm.me>
2022-03-02 13:41:26 -0700

    pdx215: Set USB product string to 'Xperia 1 III'
    Change-Id: Idda062aaa38e16d7ba65356dbca8b467fbff14c1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,5 +36,9 @@ BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # inherit from the proprietary version
 -include vendor/sony/pdx215/BoardConfigVendor.mk
